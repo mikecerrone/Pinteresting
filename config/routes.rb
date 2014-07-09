ea18f686271253de634f566407ab1451c98a7e3f
mikecerrone <mikecerrone@gmail.comgit config --global user.email mikecerrone@gmail.com>
2014-07-09 16:58:35 -0700

    added blog

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root "pages#home" #creates home page
   get "about" => "pages#about" #creates about path
+  get "blog" => "pages#blog"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
