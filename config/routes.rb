906e2aefb0f8c783f5f811e2e05055c735b52cc8
mikecerrone <mikecerrone@gmail.comgit config --global user.email mikecerrone@gmail.com>
2014-07-09 16:39:59 -0700

    UI color etc

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  root "pages#home"
-  get "about" => "pages#about"
+  root "pages#home" #creates home page
+  get "about" => "pages#about" #creates about path
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
